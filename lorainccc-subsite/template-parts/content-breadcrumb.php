29258457a3e5df5fadb2fd5cb42be39ca32aed05
Joe Querin <joecue@gmail.com>
2016-07-22 23:18:25 -0400

    Updated breadcrumbs to compensate for is_category and is_archive

--- lorainccc-subsite/template-parts/content-breadcrumb.php
+++ lorainccc-subsite/template-parts/content-breadcrumb.php
@@ -4,8 +4,12 @@
   if (function_exists('lccc_breadcrumb')){
     if (is_home() || is_front_page()) {
      echo lccc_breadcrumb() . get_bloginfo('name') ;
-    } else {
-     echo lccc_breadcrumb() . "<a href='" . get_bloginfo('url') . "'>" . get_bloginfo('name') . "</a> > " . get_the_title() ;
+    } elseif ( is_category() ) {
+    	echo lccc_breadcrumb() . "<a href='" . get_bloginfo('url') . "'>" . get_bloginfo('name') . "</a> > " . single_cat_title() ; 
+				}elseif ( is_archive() ) {
+					echo lccc_breadcrumb() . "<a href='" . get_bloginfo('url') . "'>" . get_bloginfo('name') . "</a> > " . the_archive_title() ;
+				} else {
+    	echo lccc_breadcrumb() . "<a href='" . get_bloginfo('url') . "'>" . get_bloginfo('name') . "</a> > " . get_the_title() ;
     };
    };
   ?>
